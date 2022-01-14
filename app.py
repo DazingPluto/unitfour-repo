5705f43fdb74d4a6ed55a7488b7f30b60c60690d
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2022-01-13 19:01:45 -0600

    in class notes

---
+++ app.py
@@ -0,0 +1,69 @@
+def add(num1, num2):
+
+name = "Jhonny"
+
+print(name)
+
+is_working = True
+car_off = False
+
+print(is_working)
+
+if is_working and car_off:
+    print('this is true')
+    num = 7
+    print('car is offf')
+else
+    print('this in not true')
+
+state = "Washington State"
+year = 1889
+n = 42
+my_message = f"{state} was the {n}th state to join the union"
+print(my_message)
+
+location = 'California'
+number = 6
+my_second_message = f"{location} is the {number}th largest state"
+print(my_second_message)
+
+nums = [1, 2, 3, 55, 66, 44, 33, 22, 11, 33, 750, 44, 66, 33 ]
+
+
+
+
+
+
+
+students = [
+    {
+        "name": "Kimmie",
+        "city": "Atlanta"
+    },
+     {
+        "name": "hunter",
+        "city": "Atlanta"
+    },
+     {
+        "name": "larry",
+        "city": "Georgia"
+    },
+     {
+        "name": "Hank",
+        "city": "Los Angeles"
+    },
+]
+
+for i in range(len(students)):
+    students =  students[i]
+    print(student.get("name"))
+
+    if student.get("city") = 'Los Angeles':
+        print(f'{student.get("name")} wins an Ipad for {student.get("city")}')
+
+for i in range(len(students)):
+    students =  students[i]
+    print(student.get("name"))
+
+
+
